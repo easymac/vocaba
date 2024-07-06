391ef317a63aa261ac2d3b17eac65e135be038a3
easymac <mac@ezm.ac>
2024-07-05 19:25:42 -0600

    Restructure swipe and state controls, improve animation

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,23 +1,44 @@
+import { useEffect } from 'react'
 import { StyleSheet, View, Text } from 'react-native';
 import { CardColors } from '@/constants/Colors';
 import colorInt from '@/helpers/colorInt';
-import Animated, { SharedValue, useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
+import Animated, { SharedValue, runOnJS, useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import type { Word } from '@/types';
 
 interface CardProps {
   word: Word;
-  swipeOffset: SharedValue<number> | null;
   index: number;
+  handleGesture: (direction: string) => void;
 }
-export function Card({ word, swipeOffset, index }: CardProps) {
+export function Card({ word, index, handleGesture }: CardProps) {
   const pressed = useSharedValue<boolean>(false);
   const offset = useSharedValue(0);
 
+  const springConfig = {
+    overshootClamping: true
+  }
+
+  const pan = Gesture.Pan()
+    .onChange((event) => {
+      offset.value = event.translationX;
+    })
+    .onFinalize((event) => {
+      if (event.translationX < -50) {
+        offset.value = withSpring(-500, springConfig, () => {
+          runOnJS(handleGesture)('left');
+        });
+      } else if (event.translationX > 50) {
+        offset.value = withSpring(500, springConfig, () => {
+          runOnJS(handleGesture)('right');
+        });
+      } else {
+        offset.value = withSpring(0);
+      }
+    })
+    .runOnJS(true);
+
   const animatedStyles = useAnimatedStyle(() => {
-    if (swipeOffset) {
-      offset.value = swipeOffset.value;
-    }
     return {
       transform: [
         { translateX: offset.value },
@@ -28,12 +49,15 @@ export function Card({ word, swipeOffset, index }: CardProps) {
 
   return (
     <View style={styles.wrapper}>
-      <Animated.View style={[
-        { padding: 4, height: '100%', width: '100%', backgroundColor: CardColors[colorInt(word.word)] },
-        animatedStyles
-      ]}>
-        <Text>{word.word}</Text>
-      </Animated.View>
+      <GestureDetector gesture={pan}>
+        <Animated.View style={[
+          { backgroundColor: CardColors[colorInt(word.word)] },
+          animatedStyles,
+          styles.card,
+        ]}>
+          <Text style={styles.word}>{word.word}</Text>
+        </Animated.View>
+      </GestureDetector>
     </View>
   );
 }
@@ -46,5 +70,19 @@ const styles = StyleSheet.create({
     right: 20,
     bottom: 20,
     left: 20,
+  },
+  word: {
+    fontSize: 64,
+    letterSpacing: -2,
+    opacity: 0.85,
+    fontWeight: 'bold',
+  },
+  card: {
+    padding: 4,
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    borderRadius: 70,
   }
 })
--- components/Deck.tsx
+++ components/Deck.tsx
@@ -2,72 +2,27 @@ import { useState } from 'react';
 import { View } from 'react-native';
 import { Card } from '@/components/Card';
 import DevWordBank from '@/constants/DevWordBank';
-import { Gesture, GestureDetector } from 'react-native-gesture-handler';
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withSpring,
-  withTiming
-} from 'react-native-reanimated';
 
 export function Deck() {
-  const [words, setWords] = useState(DevWordBank);
+  const [words, setWords] = useState(DevWordBank.slice(0, 10));
 
-  const pressed = useSharedValue<boolean>(false);
-  const topCardOffset = useSharedValue(0);
+  const handleGesture = (direction: string) => {
+    setWords(words.slice(1));
+  }
 
-  const pan = Gesture.Pan()
-    .onBegin(() => {
-      pressed.value = true;
-    })
-    .onChange((event) => {
-      topCardOffset.value = event.translationX;
-    })
-    .onFinalize(({ translationX }) => {
-      if (translationX < -50) {
-        // left swipe
-        topCardOffset.value = withSpring(-500, undefined, () => {
-          console.log('Left swipe')
-          setWords((prevWords) => {
-            const [top, ...rest] = prevWords;
-            return [...rest, top];
-          });
-        });
-      } else if (translationX > 50) {
-        // right swipe
-        topCardOffset.value = withSpring(500, undefined, () => {
-          console.log('Right swipe')
-          setWords((prevWords) => {
-            const [top, ...rest] = prevWords;
-            return [...rest, top];
-          });
-        
-        });
-      } else {
-        topCardOffset.value = withSpring(0);
-      }
-      pressed.value = false;
-    });
-
-  
-  const cards = words.map(
-    (word, index) => {
-      const swipeOffset = index ? null : topCardOffset;
-      if (!index) return (
-        <Card key={word.id} word={word} swipeOffset={swipeOffset} index={index} />
-      )
-      return (
-        <Card key={word.id} word={word} swipeOffset={null} index={index} />
-      )
-    }
-  ).reverse();
+  const cards = words.map((word, index) => (
+    <Card
+      key={word.id}
+      word={word}
+      handleGesture={handleGesture}
+      index={index}
+    />
+  )).reverse();
 
 
   return (
-    <GestureDetector gesture={pan}>
-      <View style={{ height: '100%', width: '100%', padding: '5%', backgroundColor: '#111' }}>
-        {cards}
-      </View>
-    </GestureDetector>
+    <View style={{ height: '100%', width: '100%', padding: '5%', backgroundColor: '#111' }}>
+      {cards}
+    </View>
   )
 }
